8ca29765c2c76d1f0572ac80554539123be323a1
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-02-24 17:14:15 +0900

    Create 7. Clustering the fish data.py

---
+++ course 2/Unsupervised Learning/Chapter 1 - Clustering for dataset exploration/7. Clustering the fish data.py
@@ -0,0 +1,23 @@
+'''
+You'll now use your standardization and clustering pipeline from the previous exercise to cluster the fish by their measurements, and then create a cross-tabulation to compare the cluster labels with the fish species.
+
+As before, samples is the 2D array of fish measurements. Your pipeline is available as pipeline, and the species of every fish sample is given by the list species.
+'''
+
+# Import pandas
+import pandas as pd
+
+# Fit the pipeline to samples
+pipeline.fit(samples)
+
+# Calculate the cluster labels: labels
+labels = pipeline.predict(samples)
+
+# Create a DataFrame with labels and species as columns: df
+df = pd.DataFrame({'labels':labels, 'species':species})
+
+# Create crosstab: ct
+ct = pd.crosstab(df['labels'], df['species'])
+
+# Display ct
+print(ct)
