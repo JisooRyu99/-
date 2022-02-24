9d5ff8562e083a8d22e9f0f83f8385b27f27747e
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-02-24 16:10:31 +0900

    Update Clustering 2D points.py

--- course 2/Unsupervised Learning/Chapter 1 - Clustering for dataset exploration/Clustering 2D points.py
+++ course 2/Unsupervised Learning/Chapter 1 - Clustering for dataset exploration/Clustering 2D points.py
@@ -1,1 +1,19 @@
+'''
+From the scatter plot of the previous exercise, you saw that the points seem to separate into 3 clusters. You'll now create a KMeans model to find 3 clusters, and fit it to the data points from the previous exercise. After the model has been fit, you'll obtain the cluster labels for some new points using the .predict() method.
 
+You are given the array points from the previous exercise, and also an array new_points.
+'''
+
+# Import KMeans
+from sklearn.cluster import KMeans
+
+# Create a KMeans instance with 3 clusters: model
+model = KMeans(n_clusters=3)
+
+# Fit model to points
+model.fit(points)
+
+# Determine the cluster labels of new_points: labels
+labels = model.predict(new_points)
+# Print cluster labels of new_points
+print(labels)
