ce37098f4ecf9b2c4d24b32ca8180ada477e022c
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-02-24 17:19:17 +0900

    Create 9. Which stocks move together?.py

---
+++ course 2/Unsupervised Learning/Chapter 1 - Clustering for dataset exploration/9. Which stocks move together?.py
@@ -0,0 +1,17 @@
+'''
+In the previous exercise, you clustered companies by their daily stock price movements. So which company have stock prices that tend to change in the same way? You'll now inspect the cluster labels from your clustering to find out.
+
+Your solution to the previous exercise has already been run. Recall that you constructed a Pipeline pipeline containing a KMeans model and fit it to the NumPy array movements of daily stock movements. In addition, a list companies of the company names is available.
+'''
+
+# Import pandas
+import pandas as pd
+
+# Predict the cluster labels: labels
+labels = pipeline.predict(movements)
+
+# Create a DataFrame aligning labels and companies: df
+df = pd.DataFrame({'labels': labels, 'companies': companies})
+
+# Display df sorted by cluster label
+print(df.sort_values('labels'))
