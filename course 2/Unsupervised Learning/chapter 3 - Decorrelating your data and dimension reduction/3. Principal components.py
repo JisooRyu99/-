4ff4f987f324d05820e2981587bce28a2f36decd
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-02-28 15:10:48 +0900

    Create 3. Principal components.py

---
+++ course 2/Unsupervised Learning/chapter 3 - Decorrelating your data and dimension reduction/3. Principal components.py
@@ -0,0 +1,14 @@
+'''
+On the right are three scatter plots of the same point cloud. Each scatter plot shows a different set of axes (in red).
+In which of the plots could the axes represent the principal components of the point cloud?
+
+Recall that the principal components are the directions along which the the data varies.
+'''
+
+Possible Answers (Answer : 2. Both plot 1 and plot 3.)
+
+1. None of them.
+
+2. Both plot 1 and plot 3.
+
+3. Plot 2.
