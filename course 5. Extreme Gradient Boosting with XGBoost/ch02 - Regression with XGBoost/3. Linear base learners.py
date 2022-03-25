3f9ca2205bd959ed5ae228337d436285e46552c6
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-03-25 17:46:29 +0900

    Create 3. Linear base learners.py

---
+++ course 5. Extreme Gradient Boosting with XGBoost/ch02 - Regression with XGBoost/3. Linear base learners.py
@@ -0,0 +1,29 @@
+'''
+Now that you've used trees as base models in XGBoost, let's use the other kind of base model that can be used with XGBoost - a linear learner. This model, although not as commonly used in XGBoost, allows you to create a regularized linear regression using XGBoost's powerful learning API. However, because it's uncommon, you have to use XGBoost's own non-scikit-learn compatible functions to build the model, such as xgb.train().
+
+In order to do this you must create the parameter dictionary that describes the kind of booster you want to use (similarly to how you created the dictionary in Chapter 1 when you used xgb.cv()). The key-value pair that defines the booster type (base model) you need is "booster":"gblinear".
+
+Once you've created the model, you can use the .train() and .predict() methods of the model just like you've done in the past.
+
+Here, the data has already been split into training and testing sets, so you can dive right into creating the DMatrix objects required by the XGBoost learning API.
+
+Instructions
+100 XP
+'''
+
+# Convert the training and testing sets into DMatrixes: DM_train, DM_test
+DM_train = xgb.DMatrix(data=X_train, label=y_train)
+DM_test =  xgb.DMatrix(data=X_test, label=y_test)
+
+# Create the parameter dictionary: params
+params = {"booster":"gblinear", "objective":"reg:linear"}
+
+# Train the model: xg_reg
+xg_reg = xgb.train(params = params, dtrain=DM_train, num_boost_round=5)
+
+# Predict the labels of the test set: preds
+preds = xg_reg.predict(DM_test)
+
+# Compute and print the RMSE
+rmse = np.sqrt(mean_squared_error(y_test,preds))
+print("RMSE: %f" % (rmse))
