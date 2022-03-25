c165a50ecc81b1fb0f68614b308257d6ea47b8c5
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-03-25 17:58:30 +0900

    Create 5. Visualizing individual XGBoost trees.py

---
+++ course 5. Extreme Gradient Boosting with XGBoost/ch02 - Regression with XGBoost/5. Visualizing individual XGBoost trees.py
@@ -0,0 +1,26 @@
+'''
+Now that you've used XGBoost to both build and evaluate regression as well as classification models, you should get a handle on how to visually explore your models. Here, you will visualize individual trees from the fully boosted model that XGBoost creates using the entire housing dataset.
+
+XGBoost has a plot_tree() function that makes this type of visualization easy. Once you train a model using the XGBoost learning API, you can pass it to the plot_tree() function along with the number of trees you want to plot using the num_trees argument
+'''
+
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary: params
+params = {"objective":"reg:linear", "max_depth":2}
+
+# Train the model: xg_reg
+xg_reg = xgb.train(params=params, dtrain=housing_dmatrix, num_boost_round=10)
+
+# Plot the first tree
+xgb.plot_tree(xg_reg, num_trees=0)
+plt.show()
+
+# Plot the fifth tree
+xgb.plot_tree(xg_reg, num_trees=4)
+plt.show()
+
+# Plot the last tree sideways
+xgb.plot_tree(xg_reg, num_trees=9, rankdir="LR")
+plt.show()
