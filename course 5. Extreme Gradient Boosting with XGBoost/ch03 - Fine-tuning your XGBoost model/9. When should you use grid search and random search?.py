0d387e0d7d18424d289b37c17f27a1352d25330f
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-03-28 02:00:55 +0900

    Create 9. When should you use grid search and random search?.py

---
+++ course 5. Extreme Gradient Boosting with XGBoost/ch03 - Fine-tuning your XGBoost model/9. When should you use grid search and random search?.py
@@ -0,0 +1,13 @@
+'''
+Now that you've seen some of the drawbacks of grid search and random search, which of the following most accurately describes why both random search and grid search are non-ideal search hyperparameter tuning strategies in all scenarios?
+'''
+
+Possible Answers = 3
+
+1. Grid Search and Random Search both take a very long time to perform, regardless of the number of parameters you want to tune.
+
+2. Grid Search and Random Search both scale exponentially in the number of hyperparameters you want to tune.
+
+3. The search space size can be massive for Grid Search in certain cases, whereas for Random Search the number of hyperparameters has a significant effect on how long it takes to run.
+
+4. Grid Search and Random Search require that you have some idea of where the ideal values for hyperparameters reside.
