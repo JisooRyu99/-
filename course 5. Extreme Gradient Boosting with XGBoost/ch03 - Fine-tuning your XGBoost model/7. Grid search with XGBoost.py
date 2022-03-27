ea6989f91207153768d9f0d1d2fd4a8e635a901c
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-03-28 01:54:37 +0900

    Create 7. Grid search with XGBoost.py

---
+++ course 5. Extreme Gradient Boosting with XGBoost/ch03 - Fine-tuning your XGBoost model/7. Grid search with XGBoost.py
@@ -0,0 +1,24 @@
+'''
+Now that you've learned how to tune parameters individually with XGBoost, let's take your parameter tuning to the next level by using scikit-learn's GridSearch and RandomizedSearch capabilities with internal cross-validation using the GridSearchCV and RandomizedSearchCV functions. You will use these to find the best model exhaustively from a collection of possible parameter values across multiple parameters simultaneously. Let's get to work, starting with GridSearchCV!
+'''
+
+# Create the parameter grid: gbm_param_grid
+gbm_param_grid = {
+    'colsample_bytree': [0.3, 0.7],
+    'n_estimators': [50],
+    'max_depth': [2, 5]
+}
+
+# Instantiate the regressor: gbm
+gbm = xgb.XGBRegressor()
+
+# Perform grid search: grid_mse
+grid_mse = GridSearchCV(param_grid=gbm_param_grid, estimator=gbm, scoring='neg_mean_squared_error', cv=4, verbose=1)
+
+
+# Fit grid_mse to the data
+grid_mse.fit(X,y)
+
+# Print the best parameters and lowest RMSE
+print("Best parameters found: ", grid_mse.best_params_)
+print("Lowest RMSE found: ", np.sqrt(np.abs(grid_mse.best_score_)))
