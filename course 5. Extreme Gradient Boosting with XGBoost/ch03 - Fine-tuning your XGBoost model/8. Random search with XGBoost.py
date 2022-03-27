9166de14c9001076fcb227a74977bf0c180062d8
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-03-28 01:56:52 +0900

    Create 8. Random search with XGBoost.py

---
+++ course 5. Extreme Gradient Boosting with XGBoost/ch03 - Fine-tuning your XGBoost model/8. Random search with XGBoost.py
@@ -0,0 +1,23 @@
+'''
+Often, GridSearchCV can be really time consuming, so in practice, you may want to use RandomizedSearchCV instead, as you will do in this exercise. The good news is you only have to make a few modifications to your GridSearchCV code to do RandomizedSearchCV. The key difference is you have to specify a param_distributions parameter instead of a param_grid parameter.
+'''
+
+# Create the parameter grid: gbm_param_grid 
+gbm_param_grid = {
+    'n_estimators': [25],
+    'max_depth': range(2, 12)
+}
+
+# Instantiate the regressor: gbm
+gbm = xgb.XGBRegressor(n_estimators=10)
+
+# Perform random search: grid_mse
+randomized_mse = RandomizedSearchCV(param_distributions=gbm_param_grid, estimator=gbm, scoring='neg_mean_squared_error', n_iter=5, cv=4, verbose=1)
+
+
+# Fit randomized_mse to the data
+randomized_mse.fit(X,y)
+
+# Print the best parameters and lowest RMSE
+print("Best parameters found: ", randomized_mse.best_params_)
+print("Lowest RMSE found: ", np.sqrt(np.abs(randomized_mse.best_score_)))
