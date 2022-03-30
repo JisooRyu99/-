4bcb2c6a84038a1a4f73bb5636b9cfc6240990cd
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-03-30 15:48:00 +0900

    Create 2. Set the tree's hyperparameter grid.py

---
+++ course 4/Machine Learning with Tree-Based Models in Python/Chapter 5 - Model Tuning/2. Set the tree's hyperparameter grid.py
@@ -0,0 +1,8 @@
+'''
+In this exercise, you'll manually set the grid of hyperparameters that will be used to tune the classification tree dt and find the optimal classifier in the next exercise.
+'''
+
+# Define params_dt
+params_dt = {
+    'max_depth':[2,3,4],
+    'min_samples_leaf':[0.12, 0.14, 0.16, 0.18]}
