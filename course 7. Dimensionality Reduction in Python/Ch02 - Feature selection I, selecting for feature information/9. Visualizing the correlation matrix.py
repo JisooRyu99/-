bce24697dd84c1e124162c13f5e4e6405aaca76d
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-04-08 22:39:24 +0900

    Update 9. Visualizing the correlation matrix.py

--- course 7. Dimensionality Reduction in Python/Ch02 - Feature selection I, selecting for feature information/9. Visualizing the correlation matrix.py
+++ course 7. Dimensionality Reduction in Python/Ch02 - Feature selection I, selecting for feature information/9. Visualizing the correlation matrix.py
@@ -26,3 +26,13 @@ mask = np.triu(np.ones_like(corr, dtype=bool))
 # Add the mask to the heatmap
 sns.heatmap(corr, mask=mask, cmap=cmap, center=0, linewidths=1, annot=True, fmt=".2f")
 plt.show()
+
+
+ansure_df.corr()
+
+Elbow rest height  Wrist circumference  Ankle circumference  Buttock height  Crotch height
+Elbow rest height             1.000000             0.294753             0.301963       -0.007013      -0.026090
+Wrist circumference           0.294753             1.000000             0.702178        0.576679       0.606582
+Ankle circumference           0.301963             0.702178             1.000000        0.367548       0.386502
+Buttock height               -0.007013             0.576679             0.367548        1.000000       0.929411
+Crotch height                -0.026090             0.606582             0.386502        0.929411       1.000000
