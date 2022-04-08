7c18441e20e4bbaada9c05e8dac2172398d19755
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-04-08 22:39:00 +0900

    Create 9. Visualizing the correlation matrix.py

---
+++ course 7. Dimensionality Reduction in Python/Ch02 - Feature selection I, selecting for feature information/9. Visualizing the correlation matrix.py
@@ -0,0 +1,28 @@
+'''
+Reading the correlation matrix of ansur_df in its raw, numeric format doesn't allow us to get a quick overview. Let's improve this by removing redundant values and visualizing the matrix using seaborn.
+
+Seaborn has been pre-loaded as sns, matplotlib.pyplot as plt, NumPy as np and pandas as pd.
+'''
+
+# Create the correlation matrix
+corr = ansur_df.corr()
+
+# Draw the heatmap
+sns.heatmap(corr,  cmap=cmap, center=0, linewidths=1, annot=True, fmt=".2f")
+plt.show()
+
+# Create the correlation matrix
+corr = ansur_df.corr()
+
+# Generate a mask for the upper triangle
+mask = np.triu(np.ones_like(corr, dtype=bool))
+
+# Create the correlation matrix
+corr = ansur_df.corr()
+
+# Generate a mask for the upper triangle 
+mask = np.triu(np.ones_like(corr, dtype=bool))
+
+# Add the mask to the heatmap
+sns.heatmap(corr, mask=mask, cmap=cmap, center=0, linewidths=1, annot=True, fmt=".2f")
+plt.show()
