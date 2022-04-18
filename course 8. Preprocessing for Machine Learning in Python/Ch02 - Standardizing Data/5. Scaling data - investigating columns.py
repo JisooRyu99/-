705a59727970daada4fe825d6b931f0a434278e6
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-04-18 13:16:59 +0900

    Create 5. Scaling data - investigating columns.py

---
+++ course 8. Preprocessing for Machine Learning in Python/Ch02 - Standardizing Data/5. Scaling data - investigating columns.py
@@ -0,0 +1,13 @@
+'''
+We want to use the Ash, Alcalinity of ash, and Magnesium columns in the wine dataset to train a linear model, but it's possible that these columns are all measured in different ways, which would bias a linear model. Using describe() to return descriptive statistics about this dataset, which of the following statements are true about the scale of data in these columns?
+'''
+
+Possible Answers = 4
+
+1. The max of Ash is 3.23, the max of Alcalinity of ash is 30, and the max of Magnesium is 162.
+
+2. The means of Ash and Alcalinity of ash are less than 20, while the mean of Magnesium is greater than 90.
+
+3. The standard deviations of Ash and Alcalinity of ash are equal.
+
+4. 1 and 2 are true.
