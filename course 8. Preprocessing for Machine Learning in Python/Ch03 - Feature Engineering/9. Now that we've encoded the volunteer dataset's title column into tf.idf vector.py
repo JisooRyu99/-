a7ed3bb41028d3ca9d70e0e3d1af76b79631ce5f
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-04-18 23:00:45 +0900

    Create 9. Now that we've encoded the volunteer dataset's title column into tf.idf vector.py

---
+++ course 8. Preprocessing for Machine Learning in Python/Ch03 - Feature Engineering/9. Now that we've encoded the volunteer dataset's title column into tf.idf vector.py
@@ -0,0 +1,13 @@
+'''
+Now that we've encoded the volunteer dataset's title column into tf/idf vectors, let's use those vectors to try to predict the category_desc column.
+'''
+
+# Split the dataset according to the class distribution of category_desc
+y = volunteer["category_desc"]
+X_train, X_test, y_train, y_test = train_test_split(text_tfidf.toarray(), y, stratify=y)
+
+# Fit the model to the training data
+nb.fit(X_train, y_train)
+
+# Print out the model's accuracy
+print(nb.score(X_test, y_test))
