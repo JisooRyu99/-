b06f308cba1d19bc7d701e0688d3af99d1b881df
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-04-19 14:40:38 +0900

    Create 1. When to use feature selection.py

---
+++ course 8. Preprocessing for Machine Learning in Python/Ch04 - Selecting features for modeling/1. When to use feature selection.py
@@ -0,0 +1,13 @@
+'''
+Let's say you had finished standardizing your data and creating new features. Which of the following scenarios is NOT a good candidate for feature selection?
+'''
+
+Possible Answers = 2
+
+1. Several columns of running times that have been averaged into a new column.
+
+2. A text field that hasn't been turned into a tf/idf vector yet.
+
+3. A categorical field that has been one-hot encoded.
+
+4. Your dataset contains columns related to whether something is a fruit or vegetable, the name of the fruit or vegetable, and the scientific name of the plant.
