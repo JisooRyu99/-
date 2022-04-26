6755f6a6cef7291d4aa97982c7aaa7f4853f9251
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-04-26 14:40:21 +0900

    Create 5. Calculating features from the envelope.py

---
+++ course 9. Machine Learning for Time Series Data in Python/Ch02 - Time Series as Inputs to a Model/5. Calculating features from the envelope.py
@@ -0,0 +1,19 @@
+'''
+Now that you've removed some of the noisier fluctuations in the audio, let's see if this improves your ability to classify.
+
+audio_rectified_smooth from the previous exercise is available in your workspace.
+'''
+
+# Calculate stats
+means = np.mean(audio_rectified_smooth, axis=0)
+stds = np.std(audio_rectified_smooth, axis=0)
+maxs = np.max(audio_rectified_smooth, axis=0)
+
+# Create the X and y arrays
+X = np.column_stack([means, stds, maxs])
+y = labels.reshape([-1, 1])
+
+# Fit the model and score on testing data
+from sklearn.model_selection import cross_val_score
+percent_score = cross_val_score(model, X, y, cv=5)
+print(np.mean(percent_score))
