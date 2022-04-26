37463074af582a8d3fce3c1eca43248b1bba932a
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-04-26 20:25:50 +0900

    Create 8. Engineering multiple rolling features at once.py

---
+++ course 9. Machine Learning for Time Series Data in Python/Ch03 - Predicting Time Series Data/8/8. Engineering multiple rolling features at once.py
@@ -0,0 +1,18 @@
+'''
+Now that you've practiced some simple feature engineering, let's move on to something more complex. You'll calculate a collection of features for your time series data and visualize what they look like over time. This process resembles how many other time series models operate.
+'''
+
+# Define a rolling window with Pandas, excluding the right-most datapoint of the window
+prices_perc_rolling = prices_perc.rolling(20, min_periods=5, closed='right')
+
+# Define the features you'll calculate for each window
+features_to_calculate = [np.min, np.max, np.mean, np.std]
+
+# Calculate these features for your rolling window object
+features = prices_perc_rolling.agg(features_to_calculate)
+
+# Plot the results
+ax = features.loc[:"2011-01"].plot()
+prices_perc.loc[:"2011-01"].plot(ax=ax, color='k', alpha=.2, lw=3)
+ax.legend(loc=(1.01, .6))
+plt.show()
