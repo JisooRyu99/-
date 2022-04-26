41c3dd556678acdfda716b2f27b8f230ccf5e008
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-04-26 20:15:17 +0900

    Create 1. Introducing the dataset.py

---
+++ course 9. Machine Learning for Time Series Data in Python/Ch03 - Predicting Time Series Data/1. Introducing the dataset.py
@@ -0,0 +1,25 @@
+'''
+As mentioned in the video, you'll deal with stock market prices that fluctuate over time. In this exercise you've got historical prices from two tech companies (Ebay and Yahoo) in the DataFrame prices. You'll visualize the raw data for the two companies, then generate a scatter plot showing how the values for each company compare with one another. Finally, you'll add in a "time" dimension to your scatter plot so you can see how this relationship changes over time.
+
+The data has been loaded into a DataFrame called prices.
+'''
+
+1. Plot the data in prices. Pay attention to any irregularities you notice.
+
+# Plot the raw values over time
+prices.plot()
+plt.show()
+
+2. Generate a scatter plot with the values of Ebay on the x-axis, and Yahoo on the y-axis. Look up the symbols for both companies from the column names of the DataFrame.
+
+# Scatterplot with one company per axis
+prices.plot.scatter('EBAY', 'YHOO')
+plt.show()
+
+3. Finally, encode time as the color of each datapoint in order to visualize how the relationship between these two variables changes.
+
+
+# Scatterplot with color relating to time
+prices.plot.scatter('EBAY', 'YHOO', c=prices.index, 
+                    cmap=plt.cm.viridis, colorbar=False)
+plt.show()
