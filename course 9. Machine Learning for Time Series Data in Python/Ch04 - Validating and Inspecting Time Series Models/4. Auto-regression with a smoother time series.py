1ad019b9aea406963c0850d50a26bff5affe93ba
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-04-26 21:01:08 +0900

    Create 4. Auto-regression with a smoother time series.py

---
+++ course 9. Machine Learning for Time Series Data in Python/Ch04 - Validating and Inspecting Time Series Models/4. Auto-regression with a smoother time series.py
@@ -0,0 +1,13 @@
+'''
+Now, let's re-run the same procedure using a smoother signal. You'll use the same percent change algorithm as before, but this time use a much larger window (40 instead of 20). As the window grows, the difference between neighboring timepoints gets smaller, resulting in a smoother signal. What do you think this will do to the auto-regressive model?
+
+prices_perc_shifted and model (updated to use a window of 40) are available in your workspace.
+'''
+
+# Visualize the output data up to "2011-01"
+fig, axs = plt.subplots(2, 1, figsize=(10, 5))
+y.loc[:'2011-01'].plot(ax=axs[0])
+
+# Run the function to visualize model's coefficients
+visualize_coefficients(model.coef_, prices_perc_shifted.columns, ax=axs[1])
+plt.show()
