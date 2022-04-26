a6d9160daae825f6064699749234dadc559789e0
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-04-26 14:29:03 +0900

    Create 3. Build a classification model.py

---
+++ course 9. Machine Learning for Time Series Data in Python/Ch02 - Time Series as Inputs to a Model/3. Build a classification model.py
@@ -0,0 +1,15 @@
+'''
+While eye-balling differences is a useful way to gain an intuition for the data, let's see if you can operationalize things with a model. In this exercise, you will use each repetition as a datapoint, and each moment in time as a feature to fit a classifier that attempts to predict abnormal vs. normal heartbeats using only the raw data.
+
+We've split the two DataFrames (normal and abnormal) into X_train, X_test, y_train, and y_test.
+'''
+
+from sklearn.svm import LinearSVC
+
+# Initialize and fit the model
+model = LinearSVC()
+model.fit(X_train, y_train)
+
+# Generate predictions and score them manually
+predictions = model.predict(X_test)
+print(sum(predictions == y_test.squeeze()) / len(y_test))
