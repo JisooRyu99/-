52f42139b80dda3d940723cda8b4852669d124f2
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-05-17 21:27:24 +0900

    Create 02. Potential problems.py

---
+++ course 11. Model Validation in Python/Ch03. Cross Validation/02. Potential problems.py
@@ -0,0 +1,18 @@
+'''
+Which of the following statements are TRUE regarding potential problems with holdout samples:
+
+A: Using different data splitting methods may lead to varying data in the final holdout samples.
+B: If you have limited data, your holdout accuracy may be misleading.
+C: There are no problems. Creating a single train and test sample is the only way to validate models.
+D: You shouldn't use holdout samples with limited data because you are limiting the potential training data.
+'''
+
+Possible Answers = 3
+
+1. A & D
+
+2. C & D
+
+3. A & B
+
+4. A, B, & D
