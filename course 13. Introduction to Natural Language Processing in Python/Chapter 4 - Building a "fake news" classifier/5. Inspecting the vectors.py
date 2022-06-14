e373e94eadb47f141b0775495951462a5a67d2c3
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-06-14 20:28:36 +0900

    Create 5. Inspecting the vectors.py

---
+++ course 13. Introduction to Natural Language Processing in Python/Chapter 4 - Building a "fake news" classifier/5. Inspecting the vectors.py
@@ -0,0 +1,26 @@
+'''
+To get a better idea of how the vectors work, you'll investigate them by converting them into pandas DataFrames.
+
+Here, you'll use the same data structures you created in the previous two exercises (count_train, count_vectorizer, tfidf_train, tfidf_vectorizer) as well as pandas, which is imported as pd.
+'''
+
+# Create the CountVectorizer DataFrame: count_df
+count_df = pd.DataFrame(count_train.A, columns=count_vectorizer.get_feature_names())
+ 
+# Create the TfidfVectorizer DataFrame: tfidf_df
+tfidf_df = pd.DataFrame(tfidf_train.A, columns=tfidf_vectorizer.get_feature_names())
+ 
+# Print the head of count_df
+print(count_df.head())
+ 
+# Print the head of tfidf_df
+print(tfidf_df.head())
+ 
+ 
+# Calculate the difference in columns: difference
+difference = set(count_df.columns) - set(tfidf_df.columns)
+print(difference)
+# set()
+ 
+# Check whether the DataFrames are equal
+print(count_df.equals(tfidf_df))
