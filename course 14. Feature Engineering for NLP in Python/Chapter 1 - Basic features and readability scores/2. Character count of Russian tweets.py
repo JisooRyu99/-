58b2ee6ef641a4dd853b9ffdef9ccbc23a400a1c
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-06-24 20:57:47 +0900

    Create 2. Character count of Russian tweets.py

---
+++ course 14. Feature Engineering for NLP in Python/Chapter 1 - Basic features and readability scores/2. Character count of Russian tweets.py
@@ -0,0 +1,13 @@
+'''
+In this exercise, you have been given a dataframe tweets which contains some tweets associated with Russia's Internet Research Agency and compiled by FiveThirtyEight.
+
+Your task is to create a new feature 'char_count' in tweets which computes the number of characters for each tweet. Also, compute the average length of each tweet. The tweets are available in the content feature of tweets.
+
+Be aware that this is real data from Twitter and as such there is always a risk that it may contain profanity or other offensive content (in this exercise, and any following exercises that also use real Twitter data).
+'''
+
+# Create a feature char_count
+tweets['char_count'] = tweets['content'].apply(len)
+
+# Print the average character count
+print(tweets['char_count'].mean())
