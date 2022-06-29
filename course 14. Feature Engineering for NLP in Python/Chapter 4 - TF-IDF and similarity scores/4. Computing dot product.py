ac08a175bb380eb64ea1278e2c0db105c914002c
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-06-29 16:54:24 +0900

    Create 4. Computing dot product.py

---
+++ course 14. Feature Engineering for NLP in Python/Chapter 4 - TF-IDF and similarity scores/4. Computing dot product.py
@@ -0,0 +1,13 @@
+'''
+In this exercise, we will learn to compute the dot product between two vectors, A = (1, 3) and B = (-2, 2), using the numpy library. More specifically, we will use the np.dot() function to compute the dot product of two numpy arrays.
+'''
+
+# Initialize numpy vectors
+A = np.array([1,3])
+B = np.array([-2,2])
+
+# Compute dot product
+dot_prod = np.dot(A, B)
+
+# Print dot product
+print(dot_prod)
