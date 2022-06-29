8bcc0e6634220d236962a2d402e57163a2dcc34c
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-06-29 16:51:11 +0900

    Create 2. tf-idf vectors for TED talks.py

---
+++ course 14. Feature Engineering for NLP in Python/Chapter 4 - TF-IDF and similarity scores/2. tf-idf vectors for TED talks.py
@@ -0,0 +1,17 @@
+'''
+In this exercise, you have been given a corpus ted which contains the transcripts of 500 TED Talks. Your task is to generate the tf-idf vectors for these talks.
+
+In a later lesson, we will use these vectors to generate recommendations of similar talks based on the transcript.
+'''
+
+# Import TfidfVectorizer
+from sklearn import TfidfVectorizer
+
+# Create TfidfVectorizer object
+vectorizer = TfidfVectorizer()
+
+# Generate matrix of word vectors
+tfidf_matrix = vectorizer.fit_transform(ted)
+
+# Print the shape of tfidf_matrix
+print(tfidf_matrix.shape)
